d9f77e8a217062f7ffd48d8ac3d635fd0e7cd597
ThePope05 <swobben05@gmail.com>
2026-01-30 15:22:50 +0100

    Moved config folder to root

--- app/require.php
+++ app/require.php
@@ -4,6 +4,6 @@ require_once 'libraries/Core.php';
 require_once 'libraries/BaseModel.php';
 require_once 'libraries/BaseController.php';
 require_once 'libraries/Database.php';
-require_once 'config/config.php';
+require_once '../config/config.php';
 
 $init = new Core();
