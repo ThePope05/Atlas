dabaaf445da662554a1949593be3f6e06029933a
ThePope05 <swobben05@gmail.com>
2026-02-03 14:23:59 +0100

    Update welcome page styling

--- public/views/Welcome.fx.php
+++ public/views/Welcome.fx.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Sansation:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./public/css/style.css">
     <title>HomePage</title>
 </head>
@@ -13,22 +16,66 @@
     <h2>Welcome to</h2>
     <div class="perspective-box">
         <div class="box">
-            <div class="front">
+            <div class="circle">
+            </div>
+            <div class="circle">
+            </div>
+            <div class="circle">
+            </div>
+            <div class="circle">
+            </div>
+            <div class="circle">
+            </div>
+            <div class="circle horizontal">
+            </div>
+            <div class="circle horizontal">
+            </div>
+            <div class="circle horizontal">
+            </div>
+            <div class="circle horizontal">
+            </div>
+            <div class="circle horizontal">
+            </div>
+            <div class="text-banner">
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+                <div class="panel"></div>
+            </div>
+            <!-- <div class="front">
                 @component('WelcomeTitle')
             </div>
             <div class="right">
                 @component('WelcomeTitle')
             </div>
             <div class="back">
-                @component('WelcomeTitle')
             </div>
             <div class="left">
-                @component('WelcomeTitle')
             </div>
             <div class="top">
             </div>
             <div class="bottom">
-            </div>
+            </div> -->
 
             <div class="shadow">
             </div>
