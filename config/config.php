bc32f27a13175d91f299c8ad768f0af1b8dcab9d
ThePope05 <swobben05@gmail.com>
2026-02-04 17:18:31 +0100

    Changed to test db

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
-define('DB_NAME', 'boerbert_db');
+define('DB_NAME', 'atlas_test_db');
 define('URLROOT', 'http://localhost:');
 define('PORT', '8000');//ONLY WORKS FOR LOCALHOST
