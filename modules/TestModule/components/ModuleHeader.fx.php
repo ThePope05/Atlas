a1d0770d984b9338cf5d321ba266bd256d2cbe25
ThePope05 <swobben05@gmail.com>
2026-02-10 20:43:25 +0100

    Removed test module

--- modules/TestModule/components/ModuleHeader.fx.php
+++
@@ -1,2 +0,0 @@
-<h3>TestModule</h3>
-<h1>It's working!</h1>
--- modules/TestModule/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Libraries\Classes\Routing\Route;
-use Modules\TestModule\Controllers\TestModuleController;
-
-Route::Get('/module-test', [TestModuleController::class, "ModulePage"]);
